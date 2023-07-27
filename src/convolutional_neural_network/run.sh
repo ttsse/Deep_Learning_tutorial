4a7a8c12504386752e3411bb4c0e356b9d07e1d3
Zhenlu Sun <zhenlu.sun@it.uu.se>
2023-07-27 16:24:56 +0200

    run python3 instead of python

--- src/convolutional_neural_network/run.sh
+++ src/convolutional_neural_network/run.sh
@@ -3,10 +3,10 @@
 # Check the value of the "task" environment variable
 if [ "$task" = "classification" ]; then
     # If "task" is "classification", run the classification.py script
-    python digit_classification.py
+    python3 digit_classification.py
 elif [ "$task" = "segmentation" ]; then
     # If "task" is "segmentation", run the segmentation.py script
-    python semantic_seg.py
+    python3 semantic_seg.py
 else
     # If "task" is neither "classification" nor "segmentation", print an error message
     echo "Unknown task: $task"
